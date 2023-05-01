c927abfb2bef1b07dcfc324ed74df9f1309a323b
Luis Camacho <angel1@csu.fullerton.edu>
2023-05-01 13:04:12 -0700

    Initial commit

---
+++ wdio.conf.js
@@ -0,0 +1,46 @@
+exports.config = {
+    runner: 'local',
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    exclude: [
+        // 'path/to/excluded/files'
+    ],
+    maxInstances: 10,
+    headless: true,
+    capabilities: [
+        {
+        maxInstances: 5,
+        browserName: 'chrome',
+        acceptInsecureCerts: true,
+        'goog:chromeOptions': {
+            args: ['headless', 'disable-gpu']
+        }
+    },
+    {
+        maxInstances: 5,
+        browserName: 'firefox',
+        acceptInsecureCerts: true,
+        'moz:firefoxOptions': {
+            args: ['-headless']
+        }
+    }
+],
+    logLevel: 'error',
+    bail: 0,
+    baseUrl: 'https://033b7a32-0a98-4b20-8e1b-f5d8ca0373ce.serverhub.practicum-services.com',
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 120000,
+    connectionRetryCount: 3,
+    services: [
+        'chromedriver', 
+        'geckodriver', 
+        'intercept', 
+    ],
+    framework: 'mocha',
+    reporters: ['spec'],
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 60000
+    },
+}

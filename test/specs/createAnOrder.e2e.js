713de7109bfc612db0c31d307492a023b403e851
Angel <123137.com@gmail.com>
2023-05-04 11:31:34 -0700

    add pause method before button in test#2

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -29,6 +29,7 @@ describe('Ordering a taxi', () => {
         const carPickerworkflow = await $(page.carPickerworkflow);
         //Checking if the workflow-subcontainer Form appears if the address is valid 
         await expect(carPickerworkflow).toBeExisting(); 
+        
     })
 
     it('Selecting Supportive plan', async () => {
@@ -36,6 +37,7 @@ describe('Ordering a taxi', () => {
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
         const SupportiveButton = await $(page.supportiveButton);
         await SupportiveButton.waitForDisplayed();
+        await browser.pause(10000);
         await SupportiveButton.click();
         await expect(SupportiveButton).toBeExisting(); 
        // await browser.pause(10000);
